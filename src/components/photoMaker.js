11dbfdcb80decf3b5d973f42b876ff1869db9c81
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-08-15 12:43:34 -0700

    added styled-components to do previous styles

--- src/components/photoMaker.js
+++ src/components/photoMaker.js
@@ -1,22 +1,41 @@
 import React from "react"
+import styled from 'styled-components'
 
 const PhotoMaker = props => {
-    const photoMakerStyle = {
-        maxWidth: '70%',
-        margin: '0 auto',
-    }
+    const PhotoOfTheDayDiv = styled.div`
+        max-width: 70%;
+        margin: 0 auto;
+        border-left: 1px dashed black;
+        border-right: 1px dashed black;
+    `
+
+    const PhotoOfTheDay = styled.img`
+        max-width: 50%;
+    `
+
+    const PhotoOfTheDayExplanation = styled.p`
+        max-width: 90%;
+        margin: 0 auto;
+        font-family: calibri;
+    `
+
+    // const photoMakerStyle = {
+    //     maxWidth: '70%',
+    //     margin: '0 auto',
+    // }
 
     const photoStyle = {
         maxWidth: '50%',
     }
 
     return (
-        <div className="nasaPOTD_div" style={photoMakerStyle}>
-            <img className="nasaPOTD_img" alt="NASA POTD" src={props.imgUrl} style={photoStyle} />
+        <PhotoOfTheDayDiv>
+            <h1>NASA Photo of the Day</h1>
+            <PhotoOfTheDay className="nasaPOTD_img" alt="NASA POTD" src={props.imgUrl} style={photoStyle} />
             <h2>{props.title}</h2>
             <p>{props.date}</p>
-            <p>{props.explanation}</p>
-        </div> 
+            <PhotoOfTheDayExplanation>{props.explanation}</PhotoOfTheDayExplanation>
+        </PhotoOfTheDayDiv> 
     )
 }
 
